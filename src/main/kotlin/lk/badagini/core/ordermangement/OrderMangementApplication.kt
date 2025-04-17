716bef4c2109b630d332642290c18ab12bab0d1a
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-04-18 00:09:24 +0530

    Initial Commit

---
+++ src/main/kotlin/lk/badagini/core/ordermangement/OrderMangementApplication.kt
@@ -0,0 +1,11 @@
+package lk.badagini.core.ordermangement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderMangementApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderMangementApplication>(*args)
+}
