a136c7f634da4ab0d6b04bbdfc7da64b6d2fb2f0
Prashan Samarathunge <prashansamarathunge@gmail.com>
2025-04-27 12:28:15 +0530

    Minor Changes

--- src/main/kotlin/lk/badagini/core/ordermangement/controller/OrderController.kt
+++ src/main/kotlin/lk/badagini/core/ordermangement/controller/OrderController.kt
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/orders")
 class OrderController(
     private val orderService: IOrderService,
     private val objectMapper: ObjectMapper,
